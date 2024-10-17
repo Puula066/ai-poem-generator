8964ee55bfa5306d4a7e5d6c063c5018f5a981e9
Puula066 <seobi817@gmail.com>
2024-10-17 15:34:58 +0200

    Update index.html, index.js and style.css

--- src/index.js
+++ src/index.js
@@ -9,12 +9,17 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
+
   let instructionsInput = document.querySelector("#user-instructioins");
   let apiKey = "d6ad63cf3f10b394t922ccb401aocfc5";
   let prompt = `Generate a poem about spirituality ${instructionsInput.value}`;
   let context =
     "You are an african spirituality poet.Your mission is to generate 5 line poem in basic HTML. Make sure you follow the user instructions. Sign the poem with 'Puula AI' inside a <strong> element";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳Generating a poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
