22d0f7cdd15f5ed9cd6f7d3e60c9f50e22d1f9d0
Puula066 <seobi817@gmail.com>
2024-10-17 14:58:58 +0200

    updated inedx.html and index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Roses are red",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructioins");
+  let apiKey = "d6ad63cf3f10b394t922ccb401aocfc5";
+  let prompt = `Generate a poem about spirituality ${instructionsInput.value}`;
+  let context =
+    "You are an african spirituality poet.Your mission is to generate 5 line poem in basic HTML. Make sure you follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
